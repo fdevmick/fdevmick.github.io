600483ad002869442e24ffb26f7325cd7ee137e7
fdevmick <143139228+fdevmick@users.noreply.github.com>
2023-09-25 18:01:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
 //arrow down annimation
 $('.btn').hover(function(){
   $('.arrow').toggleClass('bounce');
@@ -12,28 +13,31 @@ $(".btn").click(function() {
      }, 1000);
  });
 
-//Banner image annimation
-
-
-$(document).on('scroll', function(){
-    $('.project-title h2').css("left", Math.max(400 - 0.35*window.scrollY, 100) + "px");
-})
-
+//Banner parallax
 $(document).on('scroll', function(){
     $('.banner-left').css("top", Math.max(0 - 0.15*window.scrollY) + "px");
 })
   
-//parallax scroll banner
 window.addEventListener("scroll",()=>{
   document.querySelector(".banner").style.opacity = `${(-window.scrollY + 600) * .004}`
  
 })
 
-
 //parallax scroll project section
+$(document).on('scroll', function(){
+    $('.project-title h2').css("left", Math.max(400 - 0.35*window.scrollY, 100) + "px");
+})
+
 window.addEventListener("scroll",()=>{
   document.querySelector(".project").style.opacity = `${(-window.scrollY + 2500) * .004}`
 })
+
+//project Hover
+$('.project-card').hover(function(){
+  $('.project-description').toggleClass('show-description');
+})
+
+
 
 });
 
